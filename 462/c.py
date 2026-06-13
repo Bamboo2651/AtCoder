85cd547f4bf0bfc8c5b140146ff6668177937b00
Bamboo2651 <tah331925@gmail.com>
2026-06-13 21:38:21 +0900

    c 間違い

---
+++ 462/c.py
@@ -0,0 +1,26 @@
+n = int(input())
+x = [[]]
+for _ in range(n):
+    x.append(list(map(int,input().split())))
+# print(x)
+
+count = 0
+
+for i in range(1,n+1):
+    ix = x[i][0]
+    iy = x[i][1]
+    
+    hantei = True
+    
+    for j in range(1, n + 1):
+        if i == j:
+            continue
+        jx = x[j][0]
+        jy = x[j][1]
+        
+        if jx < ix and jy < iy:
+            hantei = False
+            break
+    if hantei:
+        count += 1
+print(count)
