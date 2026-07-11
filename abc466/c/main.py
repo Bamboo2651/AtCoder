67bac48d7d6ac1503c450de6594b244aa819fc4a
Bamboo2651 <tah331925@gmail.com>
2026-07-11 22:03:55 +0900

    atcoder

--- abc466/c/main.py
+++ abc466/c/main.py
@@ -1,5 +1,5 @@
 n = int(input())
-hantei = [False] * n
+hantei = [False] * (n + 2)
 
 for i in range(1, n):
     print(f"? {i} {i+1}", flush=True)
@@ -10,12 +10,16 @@ for i in range(1, n):
 ans = 0
 for i in range(1, n):
     if hantei[i]:
-        ans += 1
-
-        if i + 1 < n and hantei[i+1]:
-            print(f"? {i} {i+2}", flush=True)
+        ans += 1  
+        right = i + 2
+        while hantei[right-1]:
+            print(f"? {i} {right}", flush=True)
             user = input()
+            
             if user == "Yes":
                 ans += 1
+                right += 1  
+            else:
+                break  
 
 print(f"! {ans}", flush=True)
