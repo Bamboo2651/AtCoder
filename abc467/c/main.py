a0a9b3faae307b15761ab0f7b7a3f761196cc9d5
Bamboo2651 <tah331925@gmail.com>
2026-07-30 12:56:03 +0900

    c ＣＬＥＡＲ

--- abc467/c/main.py
+++ abc467/c/main.py
@@ -2,4 +2,29 @@ n,m = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
 
+s1_a = a.copy()
+s0_a = a.copy()
 
+s0_right_count =0
+s1_right_count = 0
+
+for i in range(1, n):
+    if i == 1:
+        if a[i-1] % 2 == 0:
+            s1_a[i-1] = a[i-1] + 1
+            s1_right_count += 1
+        else:
+            s0_a[i-1] = a[i-1] + 1
+            s0_right_count += 1
+    
+    #先頭0の右側
+    if (s0_a[i-1] + s0_a[i]) % 2 != b[i-1]:
+        s0_a[i] = s0_a[i] + 1
+        s0_right_count += 1
+
+    #先頭1の右側
+    if (s1_a[i-1] + s1_a[i]) % 2 != b[i-1]:
+        s1_a[i] = s1_a[i] + 1
+        s1_right_count += 1
+
+print(min(s0_right_count, s1_right_count))  
