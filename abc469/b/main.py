44f80be2da9d99717ab810e63464285390cf2076
tkhy2651 <tah331925@gmail.com>
2026-08-29 20:31:58 +0900

    b

--- abc469/b/main.py
+++ abc469/b/main.py
@@ -1,1 +1,14 @@
+n = int(input())
+s = input()
 
+answer = 0
+
+for i in range(n):
+    if (
+        s[i] == "x"
+        and (i == 0 or s[i - 1] == "x")
+        and (i == n - 1 or s[i + 1] == "x")
+    ):
+        answer += 1
+
+print(answer)
